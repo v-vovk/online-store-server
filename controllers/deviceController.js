e43280e8fe0c02dd4c265fbe9ac34ad2401879ac
Vlad Vovk <wolfykey@gmail.com>
2021-06-25 18:26:54 +0300

    crud, static, query params

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,7 +1,92 @@
+const uuid = require('uuid');
+const path = require('path');
+const { Device, DeviceInfo } = require('../models/models');
+const ApiError = require('../error/ApiError');
+
 class DeviceController {
-  async create(req, res) {}
-  async getAll(req, res) {}
-  async getOne(req, res) {}
+  async create(req, res, next) {
+    try {
+      let { name, price, brandId, typeId, info } = req.body;
+      const { image } = req.files;
+
+      const fileName = uuid.v4() + '.jpg';
+
+      await image.mv(path.resolve(__dirname, '..', 'static', fileName));
+
+      const device = await Device.create({
+        name,
+        price,
+        brandId,
+        typeId,
+        info,
+        image: fileName,
+      });
+
+      if (info) {
+        info = JSON.parse(info);
+
+        info.forEach((i) => {
+          DeviceInfo.create({
+            title: i.title,
+            description: i.description,
+            deviceId: device.id,
+          });
+        });
+      }
+
+      return res.json(device);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
+  }
+
+  async getAll(req, res) {
+    let { brandId, typeId, limit, page } = req.query;
+
+    page = page || 1;
+    limit = limit || 9;
+
+    let offset = page * limit - limit;
+
+    let devices;
+
+    if (!brandId && !typeId) {
+      devices = await Device.findAndCountAll({
+        limit,
+        offset,
+      });
+    } else if (brandId && !typeId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId },
+        limit,
+        offset,
+      });
+    } else if (!brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { typeId },
+        limit,
+        offset,
+      });
+    } else if (brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { typeId, brandId },
+        limit,
+        offset,
+      });
+    }
+
+    return res.json(devices);
+  }
+
+  async getOne(req, res) {
+    const { id } = req.params;
+    const device = await Device.findOne({
+      where: { id },
+      include: [{ model: DeviceInfo, as: 'info' }],
+    });
+
+    return res.json(device);
+  }
 }
 
 module.exports = new DeviceController();
--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
 
 const PORT = process.env.PORT || 5050;
 
@@ -12,6 +14,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
+
 app.use('/api', router);
 
 app.use(errorHandler);
