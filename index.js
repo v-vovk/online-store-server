43117978db1408cc65eaa1266a3c634a7136e7ff
Vlad Vovk <wolfykey@gmail.com>
2021-06-25 15:37:30 +0300

    add models

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5050;
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 const start = async () => {
   try {
