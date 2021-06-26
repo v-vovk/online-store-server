3857bb81f4ed62681eda65dbfa9679b8f1a82547
Vlad Vovk <wolfykey@gmail.com>
2021-06-26 16:16:54 +0300

    registration, login, jwt token, bcrypt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,78 @@
 const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { User, Basket } = require('../models/models');
+
+const generateJwt = (id, email, role) => {
+  return jwt.sign(
+    {
+      id,
+      email,
+      role,
+    },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: '24h',
+    }
+  );
+};
 
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
 
-  async login(req, res) {}
-
-  async check(req, res, next) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest('id not provided'));
+    if (!email || !password) {
+      return next(ApiError.badRequest('Email or password not correct'));
     }
-    res.json(id);
+
+    const candidate = await User.findOne({
+      where: { email },
+    });
+
+    if (candidate) {
+      return next(
+        ApiError.badRequest('User with this email are already exist')
+      );
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+
+    const user = await User.create({
+      email,
+      role,
+      password: hashPassword,
+    });
+
+    const basket = await Basket.create({
+      userId: user.id,
+    });
+
+    const token = generateJwt(user.id, user.email, user.role);
+
+    return res.json({ token });
   }
+
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      return next(ApiError.badRequest('User with this email not found'));
+    }
+
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.badRequest('Password not correct'));
+    }
+
+    const token = generateJwt(user.id, user.email, user.role);
+
+    return res.json({ token });
+  }
+
+  async check(req, res, next) {}
 }
 
 module.exports = new UserController();
